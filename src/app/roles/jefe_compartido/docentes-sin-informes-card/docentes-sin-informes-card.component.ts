c0d2c7cb15e08c9447ce101bacc5d6da04ff36f4
Juan Yampara <yampara64@gmail.com>
2024-10-25 16:04:13 -0300

    Se agregaron los gráficos para la visualización de alumnos en práctica y academicos con informes.

--- src/app/roles/jefe_compartido/docentes-sin-informes-card/docentes-sin-informes-card.component.ts
+++ src/app/roles/jefe_compartido/docentes-sin-informes-card/docentes-sin-informes-card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { registerables, Chart, Scale } from 'chart.js';  
+Chart.register(...registerables);
 @Component({
   selector: 'app-docentes-sin-informes-card',
   standalone: true,
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './docentes-sin-informes-card.component.html',
   styleUrl: './docentes-sin-informes-card.component.css'
 })
-export class DocentesSinInformesCardComponent {
+export class DocentesSinInformesCardComponent implements OnInit{
 
+
+  chart_docentes: any;
+  ngOnInit(): void{
+    this.chart_docentes = new Chart('chart_docentes', this.alumnos_practica());
+  }
+
+  alumnos_practica(): any {
+    return {
+      type: 'bar',
+      data: {
+        labels: ['Docentes En revisión de informes'],
+        datasets: [
+          {
+            label: 'Practica 1',
+            data: ['12'],
+            backgroundColor: 'green',
+          },
+          {
+            label: 'Practica 2',
+            data: ['10'],
+            backgroundColor: 'yellow'
+          }
+        ],
+      },
+      options: {
+        aspectRatio: 1,
+        responsive: true,
+      },
+    };
+  }
+  
 }
