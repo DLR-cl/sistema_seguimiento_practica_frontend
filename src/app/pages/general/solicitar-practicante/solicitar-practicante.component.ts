10d4c005e11b16e2323bc61d7564e85ddace99b8
juan yampara <yampara64@gmail.com>
2024-12-10 12:35:30 -0300

    add solicitar practicante correo

--- src/app/pages/general/solicitar-practicante/solicitar-practicante.component.ts
+++ src/app/pages/general/solicitar-practicante/solicitar-practicante.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class SolicitarPracticanteComponent {
 
   private _router = inject(Router);
-
+  correo:string = "ici@gestion.uta.cl";
   toHome(){
     this._router.navigate(['home']);
   }
