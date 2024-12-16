aad1ed0c34f67fcade21a07a3b0843f23826f649
zBlatzX <fabianorellana67@gmail.com>
2024-12-16 10:26:44 -0300

    style: home y solicitar practicante

--- src/app/pages/general/home/home.component.ts
+++ src/app/pages/general/home/home.component.ts
@@ -1,70 +1,45 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HeaderComponent } from "../../../roles/jefe_compartido/header-jefes/header.component";
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ImagePreloaderService } from '../../../core/services/image-preloader.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  imageUrls =[
+  imageUrls = [
     'departamento_ici/transicion_1.webp',
     'departamento_ici/transicion_2.webp',
     'departamento_ici/laboratorio_ici.webp',
     'departamento_ici/transicion_4.webp',
     'departamento_ici/transicion_5.webp',
     'departamento_ici/transicion_6.webp'
-  ]
-
-  private imagePreloaderService= inject(ImagePreloaderService);
-
-  private _router = inject(Router);
+  ];
 
   currentImageIndex: number = 0;
-  backgroundImage: string = '';
-  imagesLoaded: boolean = false;
   intervalId: any;
-  imageCache: Map<string, HTMLImageElement> = new Map();
-  animateScaleUp: boolean = true;  // Controlar la animación de escala
 
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.imagePreloaderService.preloadImages(this.imageUrls).subscribe((cache) => {
-      if (cache) {
-        this.imageCache = cache;
-        setTimeout(() => {
-          this.imagesLoaded = true;
-        }, 500);
-        this.backgroundImage = this.imageUrls[this.currentImageIndex];
-        this.startImageRotation();
-      }
-    });
+    this.startImageRotation();
   }
 
-  startImageRotation() {
+  startImageRotation(): void {
     this.intervalId = setInterval(() => {
       this.currentImageIndex = (this.currentImageIndex + 1) % this.imageUrls.length;
-      this.backgroundImage = this.imageUrls[this.currentImageIndex];
-    }, 4000);
+    }, 4000); // Cambiar cada 4 segundos
   }
 
-  getBackgroundImage(): string {
-    const img = this.imageCache.get(this.backgroundImage);
-    return img ? `url(${img.src})` : '';
+  toLogin(): void {
+    this.router.navigate(['login']);
   }
 
-  toLogin(){
-    this._router.navigate(['login']);
+  toLetter(): void {
+    this.router.navigate(['solicitar-practicante']);
   }
-  
-  toLetter(){
-    this._router.navigate(['solicitar-practicante'])
-  }
-
 }
