754dd52aad0a2d0a4f72f46e6c87ae18b0c9db2b
juan yampara <yampara64@gmail.com>
2024-12-19 15:36:49 -0300

    conexion_local

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
     production: true,
-    API_URL: 'https://seguimientopractica.vercel.app'
+    // API_URL: 'https://seguimientopractica.vercel.app'
+    API_URL: 'http://localhost:3000'
 };
